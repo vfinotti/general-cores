6a531d0952718cab763e246c61ae6fa6db7c7622
Vitor Finotti <vfinotti@ncc.unesp.br>
2018-11-01 20:21:03 -0300

    Removing assertion "Memory initialization not supported"

--- modules/genrams/xilinx/generic_spram.vhd
+++ modules/genrams/xilinx/generic_spram.vhd
@@ -216,10 +216,6 @@ architecture syn of generic_spram is
   
   
 begin
-  assert (g_init_file = "" or g_init_file = "none") 
-  report "generic_spram: Memory initialization files not supported yet. Sorry :("
-  severity failure;
-
 
   gen_with_byte_enable_writefirst : if(g_with_byte_enable = true and g_addr_conflict_resolution = "write_first") generate
     s_we <= bwe_i when we_i = '1' else (others => '0');
